5a7dacb879f0d707a762f643b64c94667072a6de
jaden.liao <jaden.liao@zoom.us>
2019-08-20 14:55:39 +0800

    add element

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
 import router from './router'
 import store from './store'
 import axios from "axios";
 
-Vue.config.productionTip = false
 Vue.prototype.$axios = axios;
+Vue.config.productionTip = false
 
 new Vue({
   router,
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   lintOnSave: false, // 保存时检查格式，使用eslint
   crossorigin: 'anonymous', // htmlWebpackPlugin
-  css: { // 对组件中css的配置
-    modules: true
-  },
-  baseUrl: '/',
+  publicPath: '/',
+  outputDir: 'dist', // 打包的目录
+  productionSourceMap: false, // 生产环境是否生成 SourceMap
   devServer: { // 对开发服务的设置
     // Various Dev Server settings
     host: '0.0.0.0', // process.env.HOST,
     port: 8081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     open: true, // 自动调用默认浏览器打开
     https: false, // 是否使用https, https使用node的一个内部默认的ca证书
+    hotOnly: false,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
